33920b41917b2562d89166d5f26710d65040b801
Nova-Aegis <pierrer1750@gmail.com>
2024-02-16 23:53:54 +0100

    maintenant x100 plus fluide

--- labo/AffichageLabo.c
+++ labo/AffichageLabo.c
@@ -7,32 +7,45 @@
 #include <SDL2/SDL_ttf.h>
 #include "AffichageLabo.h"
 
-#define SIZE_TILE_X 55
-#define SIZE_TILE_Y 48
+#define SIZE_TILE_X 105
+#define SIZE_TILE_Y 100
 
-#define WINDOW_X 1850
-#define WINDOW_Y 1050
+#define TILE_X 19
+#define TILE_Y 11
 
-#define TILE_X 35
-#define TILE_Y 23
-
+#define X_SHIFT 37
 #define Y_SHIFT 10
-#define X_SHIFT 2
 
+#define SPEED_X 2
+#define SPEED_Y 2
 
-void printMat(SDL_Renderer*renderer, int**mat, int pos_x, int pos_y, int mat_x, int mat_y){
-    int i = pos_y - TILE_Y/2 - 1;
-    int j = pos_x - TILE_X/2 - 1;
+SDL_Texture*shadow;
+
+void createShadow(SDL_Renderer*renderer){
+    SDL_Surface*surf = IMG_Load("./darken.png");
+    shadow = SDL_CreateTextureFromSurface(renderer, surf);
+    SDL_FreeSurface(surf);
+}
+void freeShadow(){
+    SDL_DestroyTexture(shadow);
+}
+
+void printMat(SDL_Renderer*renderer, unsigned**mat, int pos_x, int pos_y, int col, int row, int mat_x, int mat_y){
+    int i = row - TILE_Y/2 - 1;
+    int j = col - TILE_X/2 - 1;
+
+    int y_shift = Y_SHIFT - pos_y;
+    int x_shift = X_SHIFT - pos_x;
     
-    int i_to = pos_y + TILE_Y/2;
-    int j_to = pos_x + TILE_X/2;
+    int i_to = row + TILE_Y/2;
+    int j_to = col + TILE_X/2;
     //fprintf(stdout, "%d : %d | %d : %d | %d : %d\n", i, j, i_to, j_to,mat_y, mat_x);
-    SDL_Rect r = {-X_SHIFT, -Y_SHIFT, SIZE_TILE_X, SIZE_TILE_Y};
+    SDL_Rect r = {x_shift, y_shift, SIZE_TILE_X-2, SIZE_TILE_Y-2};
     for (int m=i;m<=i_to; m++) {
         //fprintf(stdout, "\n%d : ",m);
         if (m>=0 && m<mat_y){
             //fprintf(stdout, "in");
-            r.x = -X_SHIFT;
+            r.x = x_shift;
             for (int n=j;n<=j_to; n++) {
                 //fprintf(stdout, "%d : %d\n",m,n);
                 if (n>=0 && n<mat_x) {
@@ -67,45 +80,58 @@ void printMat(SDL_Renderer*renderer, int**mat, int pos_x, int pos_y, int mat_x, 
 
 void printPlayer(SDL_Renderer*renderer){
     SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    SDL_Rect r = {955,535,10,10};
+    SDL_Rect r = {950,530,20,20};
     SDL_RenderFillRect(renderer, &r);
 }
 
 void printShadow(SDL_Renderer*renderer){
-    
-    SDL_Surface*surf = IMG_Load("./darken.png");
-    SDL_Texture*text = SDL_CreateTextureFromSurface(renderer, surf);
     SDL_Rect r = {0,0,1920,1080};
-    SDL_RenderCopy(renderer,text, NULL,&r);
+    SDL_RenderCopy(renderer,shadow, NULL,&r);
 }
 
 
-int eventLoop(int*x,int*y){
+int eventLoop(int*vx,int*vy){
     SDL_Event event;
     while(SDL_PollEvent(&event)){
         switch (event.type) {
         case SDL_QUIT :
             return 0;
+        case SDL_KEYDOWN :
+            switch (event.key.keysym.sym){
+            case SDLK_ESCAPE :
+                return 0;
+            case SDLK_LEFT:
+                *vx = -1;
+                break;
+            case SDLK_RIGHT:
+                *vx = 1;
+                break;
+            case SDLK_UP:
+                *vy = -1;
+                break;
+            case SDLK_DOWN:
+                *vy = 1;
+                break;
+            }
+            return 1;
         case SDL_KEYUP :
             switch (event.key.keysym.sym){
             case SDLK_ESCAPE :
                 return 0;
             case SDLK_LEFT:
-                *x = *x + 1;
+                *vx = 0;
                 break;
             case SDLK_RIGHT:
-                *x = *x - 1;
+                *vx = 0;
                 break;
             case SDLK_UP:
-                *y = *y + 1;
+                *vy = 0;
                 break;
             case SDLK_DOWN:
-                *y = *y - 1;
+                *vy = 0;
                 break;
-            default :
-                return 1;
             }
-            break;
+            return 1;
         default :
             break;
         }
@@ -113,3 +139,37 @@ int eventLoop(int*x,int*y){
     return -1;
 }
 
+void calcMove(int*col,int*row,int*pos_x, int*pos_y, int vx, int vy,int nb_col, int nb_row, unsigned**mat){
+    
+    
+    if (vx) {
+        *pos_x += vx*SPEED_X;
+        if (*pos_x<0) {
+            *pos_x = *pos_x+SIZE_TILE_X;
+            *col = (*col)?
+                (*col-1):
+                0;
+        }
+        if (*pos_x>SIZE_TILE_X) {
+            *pos_x = *pos_x-SIZE_TILE_X;
+            *col = (*col)?
+                (*col+1):
+                *col;
+        }
+    }
+    if (vy) {
+        *pos_y += vy*SPEED_Y;
+        if (*pos_y<0) {
+            *pos_y = *pos_y+SIZE_TILE_Y;
+            *row = (*row)?
+                (*row-1):
+                0;
+        }
+        if (*pos_y>SIZE_TILE_Y) {
+            *pos_y = *pos_y-SIZE_TILE_Y;
+            *row = (*row)?
+                (*row+1):
+                *row;
+        }
+    }
+}
--- labo/AffichageLabo.h
+++ labo/AffichageLabo.h
@@ -2,10 +2,12 @@
 #define AFFICHAGE_LABO_H 
 #include <SDL2/SDL.h>
 
-
-void printMat(SDL_Renderer*renderer, int**mat, int pos_x, int pos_y, int mat_x, int mat_y);
+void createShadow(SDL_Renderer*renderer);
+void freeShadow();
+void printMat(SDL_Renderer*renderer, unsigned**mat, int pos_x, int pos_y, int col, int row, int mat_x, int mat_y);
 void printPlayer(SDL_Renderer*renderer);
 void printShadow(SDL_Renderer*renderer);
-int eventLoop(int*x,int*y);
+int eventLoop(int*vx,int*vy);
+void calcMove(int*col,int*row,int*pos_x, int*pos_y, int vx, int vy,int nb_col, int nb_row, unsigned**mat);
 
 #endif
